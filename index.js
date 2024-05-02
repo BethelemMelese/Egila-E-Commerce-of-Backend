e3da561f23de91fad39ccb14416d62d9c3f72101
Bethelem Melese <bethelem.m@ambftradingplc.com>
2024-05-02 12:14:33 +0300

    Update the index part and add some comment

--- index.js
+++ index.js
@@ -8,9 +8,27 @@ const app = express();
 app.use(express.json());
 dotenv.config();
 
+// Connection with Mongodb Database and run the server
+let PORT = process.env.PORT || 5000;
+mongoose
+  .connect(
+    "mongodb+srv://melesebety2673:Admin@ecommercedb.etghbjo.mongodb.net/Node-API"
+  )
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Connected to database!");
+      console.log(`Server is running on port ${PORT} ..`);
+    });
+  })
+  .catch((error) => {
+    console.log("Connection failed!", error);
+  });
+
+// the below are a await function for difference end point with their conditions
 app.get("/", (req, res) => {
   res.send("Hello from Node Api the server is included");
 });
+
 
 app.post("/api/users", async (req, res) => {
   try {
@@ -30,22 +48,20 @@ app.get("/api/users", async (req, res) => {
   }
 });
 
-let PORT = process.env.PORT || 5000;
-mongoose
-  .connect(
-    "mongodb+srv://melesebety2673:Admin@ecommercedb.etghbjo.mongodb.net/Node-API"
-  )
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT} ..`);
-    });
-    console.log("Connected to database!");
-  })
-  .catch((error) => {
-    console.log("Connection failed!", error);
-  });
+app.get("/api/users/:id", async (req,res)=>{
+  try {
+     const {id}=req.params;
+     const user=await User.findById(id);
+     console.log("The Find Add result is:",user)
+     res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({message:error.message})
+  }
+});
 
-app.post("/api/generateTOken", async (req, res) => {
+
+// this below function are help for JWT token authentication and verification
+app.post("/api/generateToken", async (req, res) => {
   try {
     let jwtSecretKey = process.env.JWT_SECRET_KEY;
     let data = {
